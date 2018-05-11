9752b6653eac9144bc181b12683afea23fda6f1e
rm5049 <38869211+rm5049@users.noreply.github.com>
2018-05-11 14:56:35 -0700

    Add files via upload

---
+++ AsteroidsPanel.java
@@ -0,0 +1,135 @@
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.AbstractAction;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.KeyStroke;
+import javax.swing.Timer;
+import javax.swing.UIManager;
+
+public class AsteroidsPanel extends JPanel {
+
+
+	Snake s = new Snake(1);
+	private Board board;
+	
+	Timer timer = new Timer(500,null);
+	public static void main(String[] args) {
+		try {
+			// Set System L&F
+			UIManager.setLookAndFeel(
+					UIManager.getSystemLookAndFeelClassName());
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		JFrame frame = new JFrame("Snake!");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		AsteroidsPanel sp = new AsteroidsPanel();
+		frame.add(sp);
+		sp.setPreferredSize(new Dimension(800,600));
+		frame.pack();
+		frame.setVisible(true);
+		sp.setUpKeyMappings();
+		sp.startGame();
+	}
+	
+	public AsteroidsPanel(){
+		board = new Board();
+		this.setPreferredSize(new Dimension(800,800));
+		//board.placeDot();
+	}
+	
+	private void setUpKeyMappings() {
+
+		this.getInputMap().put(KeyStroke.getKeyStroke("LEFT"),"left");
+		this.getActionMap().put("left",new AbstractAction(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// what do you want to do when the left arrow is pressed?
+				s.setDirection("left");
+
+				//s.move("left");
+				System.out.println("Hit left arrow!!");
+				//s.move("stop");
+			}
+		});
+		this.requestFocusInWindow();
+		
+		this.getInputMap().put(KeyStroke.getKeyStroke("RIGHT"),"right");
+		this.getActionMap().put("right",new AbstractAction(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// what do you want to do when the left arrow is pressed?
+				s.setDirection("right");
+
+				//s.move("right");
+				System.out.println("Hit right arrow!!");
+				//s.move("stop");
+			}
+		});
+		this.requestFocusInWindow();
+		
+		this.getInputMap().put(KeyStroke.getKeyStroke("UP"),"up");
+		this.getActionMap().put("up",new AbstractAction(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// what do you want to do when the left arrow is pressed?
+				s.setDirection("up");
+				//s.move("up");
+				System.out.println("Hit up arrow!!");
+				//s.move("stop");
+			}
+		});
+		this.requestFocusInWindow();
+		
+		this.getInputMap().put(KeyStroke.getKeyStroke("DOWN"),"down");
+		this.getActionMap().put("down",new AbstractAction(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// what do you want to do when the left arrow is pressed?
+				s.setDirection("down");
+				//s.move("down");
+				System.out.println("Hit down arrow!!");
+				//s.move("stop");
+			}
+		});
+		this.requestFocusInWindow();
+		
+	}
+	private void startGame() {
+		timer.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				tick();
+			}
+			
+		});
+		timer.start();
+	}
+	
+	protected void tick() {
+		System.out.println("Timer went off!");
+		s.move(s.getDirection());
+		board.getSnakeLoc();
+		board.placeDot();
+		//s.move("left");
+	}
+	
+	@Override
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+		board.drawBoard(g);
+		repaint();
+	}
+	
+}
---
+++ Ship.java
@@ -0,0 +1,103 @@
+import java.awt.List;
+import java.util.ArrayList;
+
+public class Ship {
+	private int length;
+	private String direct;
+	private int x;
+	private int y;
+	
+	ArrayList<Integer> LocationsX = new ArrayList<Integer>();
+	ArrayList<Integer> LocationsY = new ArrayList<Integer>();
+	ArrayList<Integer> LocationsXCopy;
+	ArrayList<Integer> LocationsYCopy;
+	
+	//LocationsX.add(10);
+	//LocationsY.add(10);
+
+	private static int WIDTH = 20;
+	private static int LENGTH = 20;
+	
+	public Ship(int l) {
+		length = l;
+		//x = (int) (Math.random() * 20);
+		//y = (int) (Math.random() * 20);
+		x = 10;
+		y = 10;
+		this.setDirection("left");
+		LocationsX.add(x);
+		LocationsY.add(y);
+
+	}
+	
+
+	
+	public void move(String direction) {
+		LocationsX.add(0,x);
+		LocationsY.add(0,y);
+		
+		System.out.println("[" + LocationsX.get(0) + "," + LocationsY.get(0) + "]");
+		
+		direct = direction;
+		
+		if(direct.compareTo("left") == 0) {
+				if(x-1 >= 0) {
+					x--;
+					//System.out.println(x);
+				}
+				else {
+					System.out.println("hit wall");
+				}
+				
+		}
+		else if(direct.compareTo("right") == 0) {
+			if(x+1 <= WIDTH) {
+				x++;
+				//System.out.println(x);
+			}
+			else {
+				System.out.println("hit wall");
+			}
+		}
+		else if(direct.compareTo("up") == 0) {
+			if(y+1 <= LENGTH) {
+				y++;
+				//System.out.println(x);
+			}
+			else {
+				System.out.println("hit wall");
+			}
+		}
+		else if(direct.compareTo("down") == 0) {
+			if(y-1 >= 0) {
+				y--;
+				//System.out.println(x);
+			}
+			else {
+				System.out.println("hit wall");
+			}
+		}
+	}
+	
+	public String getDirection() {
+		return direct;
+	}
+	public void setDirection(String directi) {
+		direct = directi;
+	}
+	
+	public ArrayList<Integer> getLocationsX() {		
+		LocationsXCopy= new ArrayList<Integer> (LocationsX.subList(0, length));
+		return LocationsXCopy;
+	}
+	public ArrayList<Integer> getLocationsY() {		
+		LocationsYCopy = new ArrayList<Integer> (LocationsY.subList(0, length));
+		return LocationsYCopy;
+	}
+	
+	public boolean eatDot() {
+		length++;
+		return true;
+	}
+	
+}
